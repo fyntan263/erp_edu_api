b3fd80c8e37a5e50d43d00320b5634bc64438e92
fyntan263 <samuelfyntan@gmail.com>
2025-09-13 19:39:07 +0200

    --

---
+++ src/main/java/com/innoverse/erp_edu_api/provisioning/web/dto/ProvisioningRequest.java
@@ -0,0 +1,6 @@
+package com.innoverse.erp_edu_api.provisioning.web.dto;
+
+import com.innoverse.erp_edu_api.common.domain.AcademicLevel;
+
+public record ProvisioningRequest(AcademicLevel level) {
+}
